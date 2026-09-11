b5d49666823f95f6d1c2f46a7d61bce413a40442
SiamakSm <siamak036@gmail.com>
2026-09-11 10:18:50 +0200

    3.4 - variables et fonctions interpolation

--- src/app/etudiant/etudiant.component.ts
+++ src/app/etudiant/etudiant.component.ts
@@ -7,6 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './etudiant.component.css'
 })
 export class EtudiantComponent {
+  
+  retNomEtu() {
+    return 'Bon';
+  }
+  retPrenomEtu() {
+    return 'Jean';
+  }
+
   nomEtu = 'John';
   prenomEtu = 'Wick';
 }
