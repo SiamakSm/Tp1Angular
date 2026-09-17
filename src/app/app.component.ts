70fbd318a4d860f9a2b6ce98d3cf1495a8d5f97e
SiamakSm <siamak036@gmail.com>
2026-09-17 15:09:09 +0200

    1.3 - two-way binding FormsModule ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EtudiantComponent } from './etudiant/etudiant.component';
-
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, EtudiantComponent],
+  imports: [RouterOutlet, EtudiantComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -13,6 +13,8 @@ export class AppComponent {
   title = 'Module Champ d\'applications';
   resume = 'Prise en main d\'Angular, première application';
   estAuth = false;
+
+  commentaireSeance = ''
   constructor() {
     setTimeout(() => {
       this.estAuth = true;
